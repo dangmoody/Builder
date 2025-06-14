70870fab8359834cdc63559d36805b9ecc76628f
dan <daniel.guy.moody@gmail.com>
2025-06-14 03:26:15 +0100

    better and much tidier SDL 3 build.cpp

--- demos/SDL3-3.2.14/build.cpp
+++ demos/SDL3-3.2.14/build.cpp
@@ -3,18 +3,21 @@
 #include <builder.h>
 
 BUILDER_CALLBACK void set_builder_options( BuilderOptions* options ) {
-	const std::string sdlBinaryName = "SDL";
-	const std::string sdlBinaryFolder = "bin/demos/SDL3";
+	const std::string sdl_binary_name = "SDL";
+	const std::string sdl_binary_folder = "bin/demos/SDL3";
 
-	BuildConfig configSDLCommon = {
-		.binary_name	= sdlBinaryName,
-		.binary_folder	= sdlBinaryFolder,
+	BuildConfig config_sdl_common = {
+		.binary_name	= sdl_binary_name,
+		.binary_folder	= sdl_binary_folder,
 		.binary_type	= BINARY_TYPE_DYNAMIC_LIBRARY,
 		.source_files = {
 			"src/*.c",
 			"src/atomic/*.c",
 			"src/audio/*.c",
+			"src/audio/disk/*.c",
+			"src/audio/dummy/*.c",
 			"src/camera/*.c",
+			"src/camera/dummy/*.c",
 			"src/camera/mediafoundation/*.c",
 			"src/core/*.c",
 			"src/cpuinfo/*.c",
@@ -37,9 +40,11 @@ BUILDER_CALLBACK void set_builder_options( BuilderOptions* options ) {
 			"src/power/*.c",
 			"src/process/*.c",
 			"src/render/*.c",
+			"src/render/software/*.c",
 			"src/sensor/*.c",
 			"src/stdlib/*.c",
 			"src/storage/*.c",
+			"src/storage/generic/*.c",
 			"src/tray/*.c",
 			"src/thread/*.c",
 			"src/time/*.c",
@@ -51,7 +56,6 @@ BUILDER_CALLBACK void set_builder_options( BuilderOptions* options ) {
 		},
 		.defines = {
 			"DLL_EXPORT",
-			"SDL_PLATFORM_WIN32",
 		},
 		.additional_includes = {
 			"src",	// this feels dirty, are we sure we want to do this?
@@ -61,76 +65,76 @@ BUILDER_CALLBACK void set_builder_options( BuilderOptions* options ) {
 	};
 
 	// windows
-	BuildConfig configSDLWin64 = configSDLCommon;
-	configSDLWin64.name = "sdl-windows";
-	configSDLWin64.source_files.push_back( "src/audio/dummy/*.c" );
-	configSDLWin64.source_files.push_back( "src/audio/disk/*.c" );
-	configSDLWin64.source_files.push_back( "src/audio/directsound/*.c" );
-	configSDLWin64.source_files.push_back( "src/audio/wasapi/*.c" );
-	configSDLWin64.source_files.push_back( "src/camera/dummy/*.c" );
-	configSDLWin64.source_files.push_back( "src/core/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/dialog/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/filesystem/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/gpu/vulkan/*.c" );
-	configSDLWin64.source_files.push_back( "src/gpu/d3d12/*.c" );
-	configSDLWin64.source_files.push_back( "src/haptic/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/io/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/joystick/gdk/*.c" );
-	configSDLWin64.source_files.push_back( "src/joystick/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/loadso/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/locale/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/main/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/misc/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/power/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/process/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/direct3d/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/direct3d11/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/direct3d12/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/gpu/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/opengl/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/opengles2/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/software/*.c" );
-	configSDLWin64.source_files.push_back( "src/render/vulkan/*.c" );
-	configSDLWin64.source_files.push_back( "src/sensor/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/storage/generic/*.c" );
-	configSDLWin64.source_files.push_back( "src/storage/steam/*.c" );
-	configSDLWin64.source_files.push_back( "src/thread/generic/SDL_syscond.c" );
-	configSDLWin64.source_files.push_back( "src/thread/generic/SDL_sysrwlock.c" );
-	configSDLWin64.source_files.push_back( "src/thread/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/time/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/timer/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/tray/windows/*.c" );
-	configSDLWin64.source_files.push_back( "src/video/windows/*.c" );
-	// configSDLWin64.source_files.push_back( "src/**/directsound/*.c" );
-	// configSDLWin64.source_files.push_back( "src/**/gdk/*.c" );
-	// configSDLWin64.source_files.push_back( "src/**/wasapi/*.c" );
-	// configSDLWin64.source_files.push_back( "src/**/windows/*.c" );
-	configSDLWin64.defines.push_back( "HAVE_MODF" );
-	configSDLWin64.additional_libs.push_back( "Ole32.lib" );
-	configSDLWin64.additional_libs.push_back( "OleAut32.lib" );
-	configSDLWin64.additional_libs.push_back( "Winmm.lib" );
-	configSDLWin64.additional_libs.push_back( "Imm32.lib" );
-	configSDLWin64.additional_libs.push_back( "Advapi32.lib" );
-	configSDLWin64.additional_libs.push_back( "Shell32.lib" );
-	configSDLWin64.additional_libs.push_back( "Cfgmgr32.lib" );
-	configSDLWin64.additional_libs.push_back( "Gdi32.lib" );
-	configSDLWin64.additional_libs.push_back( "SetupAPI.lib" );
-	configSDLWin64.additional_libs.push_back( "Version.lib" );
-	add_build_config( options, &configSDLWin64 );
+	// TODO(DM): 14/06/2025: the ONLY reason we cant just do "src/**/windows/*.c" here is because "hidapi/windows/hid.c" includes "hidapi_descriptor_reconstruct.c"
+	// so we have to include every windows subfolder manually whilst making sure to exclude only that one file
+	// very annoying!
+	BuildConfig config_sdl_windows = config_sdl_common;
+	config_sdl_windows.name = "sdl-windows";
+	config_sdl_windows.source_files.insert( config_sdl_windows.source_files.end(), {
+		"src/audio/directsound/*.c",
+		"src/audio/wasapi/*.c",
+		"src/core/windows/*.c",
+		"src/dialog/windows/*.c",
+		"src/filesystem/windows/*.c",
+		"src/haptic/windows/*.c",
+		"src/hidapi/windows/hid.c",
+		"src/io/windows/*.c",
+		"src/joystick/windows/*.c",
+		"src/gpu/vulkan/*.c",
+		"src/gpu/d3d12/*.c",
+		"src/loadso/windows/*.c",
+		"src/locale/windows/*.c",
+		"src/main/windows/*.c",
+		"src/misc/windows/*.c",
+		"src/power/windows/*.c",
+		"src/process/windows/*.c",
+		"src/render/direct3d/*.c",
+		"src/render/direct3d11/*.c",
+		"src/render/direct3d12/*.c",
+		"src/render/gpu/*.c",
+		"src/render/opengl/*.c",
+		"src/render/opengles2/*.c",
+		"src/render/vulkan/*.c",
+		"src/sensor/windows/*.c",
+		"src/time/windows/*.c",
+		"src/timer/windows/*.c",
+		"src/thread/generic/SDL_syscond.c",
+		"src/thread/generic/SDL_sysrwlock.c",
+		"src/thread/windows/*.c",
+		"src/tray/windows/*.c",
+		"src/video/windows/*.c"
+	} );
+	config_sdl_windows.defines.insert( config_sdl_windows.defines.end(), {
+		"SDL_PLATFORM_WIN32",
+		"HAVE_MODF"
+	} );
+	config_sdl_windows.additional_libs.insert( config_sdl_windows.additional_libs.end(), {
+		"Ole32.lib",
+		"OleAut32.lib",
+		"Winmm.lib",
+		"Imm32.lib",
+		"Advapi32.lib",
+		"Shell32.lib",
+		"Cfgmgr32.lib",
+		"Gdi32.lib",
+		"SetupAPI.lib",
+		"Version.lib"
+	} );
+	add_build_config( options, &config_sdl_windows );
 
-	BuildConfig configDemoWindows = {
+	BuildConfig config_demo_windows = {
 		.name					= "sdl-demo-windows",
-		.depends_on				= { configSDLWin64 },
+		.depends_on				= { config_sdl_windows },
 		.binary_name			= "sdl-demo-app",
-		.binary_folder			= sdlBinaryFolder,
+		.binary_folder			= sdl_binary_folder,
 		.source_files			= { "demo-app/*.cpp" },
 		.additional_includes	= { "include" },
-		.additional_lib_paths	= { sdlBinaryFolder },
-		.additional_libs		= { sdlBinaryName },
+		.additional_lib_paths	= { sdl_binary_folder },
+		.additional_libs		= { sdl_binary_name },
 		.warnings_as_errors		= true,
 	};
 
-	add_build_config( options, &configDemoWindows );
+	add_build_config( options, &config_demo_windows );
 }
 
 #endif // BUILDER_DOING_USER_CONFIG_BUILD

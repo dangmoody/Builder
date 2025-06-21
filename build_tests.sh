7a6e4fde20d51b993e82d77835661dc785cee3be
Mike430 <mikedotcoder@gmail.com>
2025-06-21 16:52:00 +0100

    moved and renamed the build tests script and specified the optimization level to bar any optimization on testing

---
+++ build_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+args="clang -O0 -lstdc++ -luuid -std=c++20 -fexceptions -ferror-limit=0 -o BuilderTests.exe tests/tests_main.cpp -DCORE_SUC -DHLML_NAMESPACE -DCORE_USE_SUBPROCESS -I src/core/include"
+echo $args
+$args
--- tests/built_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-args="clang -lstdc++ -luuid -std=c++20 -fexceptions -ferror-limit=0 -o BuilderTests.exe tests_main.cpp -DCORE_SUC -DHLML_NAMESPACE -DCORE_USE_SUBPROCESS -I ../src/core/include"
-echo $args
-$args

ceffac2423bc15f37ea87dac8b8fb4274d1eb1e2
Dan Moody <daniel.guy.moody@gmail.com>
2025-06-24 23:37:29 +0100

    fix tests that gave warnings

--- tests/test_static_lib/lib/lib.cpp
+++ tests/test_static_lib/lib/lib.cpp
@@ -1,18 +1,6 @@
 #include "lib.h"
 
-#include "../../../builder.h"
-
 #include <stdio.h>
-
-BUILDER_CALLBACK void set_builder_options( BuilderOptions* options ) {
-	BuildConfig config = {
-		.binary_folder	= "bin",
-		.binary_name	= "test_static_lib",
-		.binary_type	= BINARY_TYPE_STATIC_LIBRARY,
-	};
-
-	options->configs.push_back( config );
-}
 
 void SayHello() {
 	printf( "Hello from the static library!\n" );

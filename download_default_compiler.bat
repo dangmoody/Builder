a85c0a2a774b1333df8793b2b46e7a1c85208352
Dan Moody <daniel.guy.moody@gmail.com>
2025-07-21 00:30:25 +0100

    fix download script

--- download_default_compiler.bat
+++ download_default_compiler.bat
@@ -22,34 +22,36 @@ if not exist clang_linux (
 )
 
 echo Downloading Clang win64 archive...
-curl -o %clang_win64%\\clang_%clang_version%_win64.tar.xz -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-%clang_version%/clang+llvm-%clang_version%-x86_64-pc-windows-msvc.tar.xz"
+curl -o clang_win64\\clang_%clang_version%_win64.tar.xz -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-%clang_version%/clang+llvm-%clang_version%-x86_64-pc-windows-msvc.tar.xz"
 echo Done.
 echo.
 
 echo Downloading Clang linux archive...
-curl -o %clang_linux%\\clang_%clang_version%_linux.tar.xz -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-%clang_version%/LLVM-%clang_version%-Linux-X64.tar.xz"
+curl -o clang_linux\\clang_%clang_version%_linux.tar.xz -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-%clang_version%/LLVM-%clang_version%-Linux-X64.tar.xz"
 echo Done.
 echo.
 
-REM REM unarchive curl downloads
-REM echo Unarchiving Clang win64 archive...
-REM .\\7zip\\7za.exe x "%temp_folder%/clang_%clang_version%_win64.tar.xz" -o"clang_win64"
-REM .\\7zip\\7za.exe x "%temp_folder%/clang_%clang_version%_win64.tar" -o"clang_win64"
-REM robocopy /nfl /ndl /e "clang_win64\\clang+llvm-%clang_version%-x86_64-pc-windows-msvc" clang_win64
-REM rmdir /s /q "clang_win64\\clang+llvm-%clang_version%-x86_64-pc-windows-msvc"
-REM echo Done.
-REM echo.
+REM unarchive curl downloads
+echo Unarchiving Clang win64 archive...
+.\\7zip\\7za.exe x "clang_win64/clang_%clang_version%_win64.tar.xz" -o"clang_win64"
+.\\7zip\\7za.exe x "clang_win64/clang_%clang_version%_win64.tar" -o"clang_win64"
+robocopy /nfl /ndl /e "clang_win64\\clang+llvm-%clang_version%-x86_64-pc-windows-msvc" clang_win64
+rmdir /s /q "clang_win64\\clang+llvm-%clang_version%-x86_64-pc-windows-msvc"
+echo Done.
+echo.
 
-REM echo Unarchiving Clang linux archive...
-REM .\\7zip\\7za.exe x "%temp_folder%/clang_%clang_version%_linux.tar.xz" -o"clang_linux"
-REM .\\7zip\\7za.exe x "%temp_folder%/clang_%clang_version%_linux.tar" -o"clang_linux"
-REM robocopy /nfl /ndl /e "clang_linux\\LLVM-20.1.5-Linux-X64" clang_linux
-REM rmdir /s /q "clang_linux\\LLVM-20.1.5-Linux-X64"
-REM echo Done.
-REM echo.
+echo Unarchiving Clang linux archive...
+.\\7zip\\7za.exe x "clang_linux/clang_%clang_version%_linux.tar.xz" -o"clang_linux"
+.\\7zip\\7za.exe x "clang_linux/clang_%clang_version%_linux.tar" -o"clang_linux"
+robocopy /nfl /ndl /e "clang_linux\\LLVM-20.1.5-Linux-X64" clang_linux
+rmdir /s /q "clang_linux\\LLVM-20.1.5-Linux-X64"
+echo Done.
+echo.
 
 REM cleanup - delete the temp files
-REM del /q "%clang_win64%\\clang_%clang_version%_win64.tar.xz"
-REM del /q "%clang_linux%\\clang_%clang_version%_linux.tar.xz"
+del /q "clang_win64\\clang_%clang_version%_win64.tar"
+del /q "clang_win64\\clang_%clang_version%_win64.tar.xz"
+del /q "clang_linux\\clang_%clang_version%_linux.tar"
+del /q "clang_linux\\clang_%clang_version%_linux.tar.xz"
 
 echo Done!

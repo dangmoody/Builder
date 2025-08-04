52749422fe343fc773bc82715638c999dd594615
Dan Moody <daniel.guy.moody@gmail.com>
2025-08-04 22:32:09 +0100

    this doesnt look good, refactor it back to how it was before

--- tests/test_compiler_path/test_compiler_path.cpp
+++ tests/test_compiler_path/test_compiler_path.cpp
@@ -4,27 +4,18 @@
 
 #include <builder.h>
 
-#define DEFAULT			0
-#define CLANG			1
-#define MSVC			2
-#define GCC				3
-
-#define USE_COMPILER	MSVC
-
 BUILDER_CALLBACK void set_builder_options( BuilderOptions* options ) {
-#if USE_COMPILER == CLANG
 	options->compiler_path = "C:\\Program Files\\LLVM\\bin\\clang.exe";
 	options->compiler_version = "20.1.5";
-#elif USE_COMPILER == MSVC
+
 	// MSVC compiler path can be absolute
 	//options->compiler_path = "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.44.35207\\bin\\Hostx64\\x64\\cl.exe";
 	// but we recommend just doing this (in combination with compiler_version below) and letting builder figure out what version of MSVC you want
-	options->compiler_path = "cl";
-	options->compiler_version = "14.44.35207";
-#elif USE_COMPILER == GCC
-	options->compiler_path = "C:\\Program Files\\mingw64\\bin\\gcc.exe";
-	options->compiler_version = "15.1.0";
-#endif
+	//options->compiler_path = "cl";
+	//options->compiler_version = "14.44.35207";
+
+	//options->compiler_path = "C:\\Program Files\\mingw64\\bin\\gcc.exe";
+	//options->compiler_version = "15.1.0";
 }
 
 #endif // BUILDER_DOING_USER_CONFIG_BUILD

009675e54a374c6902ce99fe5b32002edb014a3d
Dan Moody <daniel.guy.moody@gmail.com>
2025-10-22 23:54:41 +0100

    dont download and install clang linux binaries on windows

--- download_default_compiler.bat
+++ download_default_compiler.bat
@@ -17,17 +17,8 @@ if not exist clang_win64 (
 	mkdir clang_win64
 )
 
-if not exist clang_linux (
-	mkdir clang_linux
-)
-
 echo Downloading Clang win64 archive...
 curl -o clang_win64\\clang_%clang_version%_win64.tar.xz -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-%clang_version%/clang+llvm-%clang_version%-x86_64-pc-windows-msvc.tar.xz"
-echo Done.
-echo.
-
-echo Downloading Clang linux archive...
-curl -o clang_linux\\clang_%clang_version%_linux.tar.xz -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-%clang_version%/LLVM-%clang_version%-Linux-X64.tar.xz"
 echo Done.
 echo.
 
@@ -40,18 +31,8 @@ rmdir /s /q "clang_win64\\clang+llvm-%clang_version%-x86_64-pc-windows-msvc"
 echo Done.
 echo.
 
-echo Unarchiving Clang linux archive...
-.\\7zip\\7za.exe x "clang_linux/clang_%clang_version%_linux.tar.xz" -o"clang_linux"
-.\\7zip\\7za.exe x "clang_linux/clang_%clang_version%_linux.tar" -o"clang_linux"
-robocopy /nfl /ndl /e "clang_linux\\LLVM-20.1.5-Linux-X64" clang_linux
-rmdir /s /q "clang_linux\\LLVM-20.1.5-Linux-X64"
-echo Done.
-echo.
-
 REM cleanup - delete the temp files
 del /q "clang_win64\\clang_%clang_version%_win64.tar"
 del /q "clang_win64\\clang_%clang_version%_win64.tar.xz"
-del /q "clang_linux\\clang_%clang_version%_linux.tar"
-del /q "clang_linux\\clang_%clang_version%_linux.tar.xz"
 
 echo Done!

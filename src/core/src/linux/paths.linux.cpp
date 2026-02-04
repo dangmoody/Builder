0793940efae0f7d65804a4b78cadb6f7f0f0eb31
Dan Moody <daniel.guy.moody@gmail.com>
2026-02-04 22:08:57 +0000

    fix path_current_working_directory() returning garbage data

--- src/core/src/linux/paths.linux.cpp
+++ src/core/src/linux/paths.linux.cpp
@@ -65,16 +65,23 @@ const char* path_app_path() {
 }
 
 const char* path_current_working_directory() {
-	char temp[PATH_MAX];
-
-	const char* cwd = getcwd( temp, sizeof( temp ) );
-
-	if ( !cwd ) {
+	char* cwd = getcwd( NULL, PATH_MAX * sizeof( char ) );
+	if ( cwd == NULL ) {
 		int err = errno;
 		fatal_error( "Failed to get CWD: %s.\n", strerror( err ) );
 	}
 
+#if 1
 	return cwd;
+#else
+	u64 cwdLength = strlen( cwd ) * sizeof( char );
+
+	char* result = cast( char*, mem_temp_alloc( cwdLength + 1 ) );
+	strncpy( result, cwd, cwdLength );
+	result[cwdLength] = 0;
+
+	return result;
+#endif
 }
 
 const char* path_absolute_path( const char* file ) {

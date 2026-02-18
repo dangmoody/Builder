ec8e5b28a43b66ed778cdfeeea053effe9a33d9e
Dan Moody <daniel.guy.moody@gmail.com>
2026-02-18 22:30:37 +0000

    link to clang properly on linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -45,20 +45,21 @@ defines="-D_CRT_SECURE_NO_WARNINGS -DCORE_USE_XXHASH -DCORE_SUC -DCORE_USE_SUBPR
 if [[ "$config" == "debug" ]]; then
 	programName=builder_$config
 	defines="$defines -D_DEBUG -DBUILDER_PROGRAM_NAME=\"$programName\""
-fi
-
-if [[ "$config" == "release" ]]; then
+elif [[ "$config" == "release" ]]; then
 	programName=builder
 	defines="$defines -DNDEBUG -DBUILDER_PROGRAM_NAME=\"$programName\""
 fi
 
-includes="-I${builderDir}/src/core/include"
+includes="-I${builderDir}/src/core/include -I${builderDir}/clang/include"
 
-libraries="-lstdc++ -luuid"
+libPaths="-L${builderDir}/clang/lib"
+libraries="-lstdc++ -luuid -lclang"
 
 warningLevels="-Werror -Wall -Wextra -Weverything -Wpedantic"
-ignoreWarnings="-Wno-newline-eof -Wno-format-nonliteral -Wno-gnu-zero-variadic-macro-arguments -Wno-declaration-after-statement -Wno-unsafe-buffer-usage -Wno-zero-as-null-pointer-constant -Wno-c++98-compat-pedantic -Wno-old-style-cast -Wno-missing-field-initializers -Wno-switch-default -Wno-covered-switch-default -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-cast-align -Wno-double-promotion -Wno-alloca -Wno-padded"
+ignoreWarnings="-Wno-newline-eof -Wno-format-nonliteral -Wno-gnu-zero-variadic-macro-arguments -Wno-declaration-after-statement -Wno-unsafe-buffer-usage -Wno-zero-as-null-pointer-constant -Wno-c++98-compat-pedantic -Wno-old-style-cast -Wno-missing-field-initializers -Wno-switch-default -Wno-covered-switch-default -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-cast-align -Wno-double-promotion -Wno-alloca -Wno-padded -Wno-documentation-unknown-command"
 
-args="${clangDir}/bin/clang -std=c++20 -ferror-limit=0 -o $binFolder/$programName $symbols $optimisation $sourceFiles $defines $includes $libraries $warningLevels $ignoreWarnings"
+args="${clangDir}/bin/clang -std=c++20 -ferror-limit=0 -o $binFolder/$programName $symbols $optimisation $sourceFiles $defines $includes $libPaths $libraries $warningLevels $ignoreWarnings -Wl,-rpath=$binFolder"
 echo $args
 $args
+
+cp ${clangDir}/lib/libclang.so ${binFolder}
--- scripts/build_tests.sh
+++ scripts/build_tests.sh
@@ -36,13 +36,21 @@ sourceFiles="tests/tests_main.cpp src/builder.cpp src/visual_studio.cpp src/back
 
 defines="-DCORE_SUC -DHLML_NAMESPACE -DCORE_USE_SUBPROCESS -DHASHMAP_HIDE_MISSING_KEY_WARNING"
 if [[ "$config" == "debug" ]]; then
-	programName=\"builder_$config\"
-	defines="$defines -D_DEBUG -DBUILDER_PROGRAM_NAME=$programName"
+	programName=builder_$config
+	defines="$defines -D_DEBUG -DBUILDER_PROGRAM_NAME=\"$programName\""
 elif [[ "$config" == "release" ]]; then
-	programName=\"builder\"
-	defines="$defines -DNDEBUG -DBUILDER_PROGRAM_NAME=$programName"
+	programName=builder
+	defines="$defines -DNDEBUG -DBUILDER_PROGRAM_NAME=\"$programName\""
 fi
 
-args="${clangDir}/bin/clang ${symbols} ${optimization} -lstdc++ -luuid -std=c++20 -fexceptions -ferror-limit=0 -o ${binFolder}/builder_tests_${config} ${sourceFiles} ${defines} -Isrc/core/include"
+includes="-I${builderDir}/src/core/include -I${builderDir}/clang/include"
+
+libPaths="-L${builderDir}/clang/lib"
+libs="-lstdc++ -luuid -lclang"
+
+warningLevels="-Werror -Wall -Wextra -Weverything -Wpedantic"
+ignoreWarnings="-Wno-newline-eof -Wno-format-nonliteral -Wno-gnu-zero-variadic-macro-arguments -Wno-declaration-after-statement -Wno-unsafe-buffer-usage -Wno-zero-as-null-pointer-constant -Wno-c++98-compat-pedantic -Wno-old-style-cast -Wno-missing-field-initializers -Wno-switch-default -Wno-covered-switch-default -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-cast-align -Wno-double-promotion -Wno-alloca -Wno-padded -Wno-documentation-unknown-command"
+
+args="${clangDir}/bin/clang ${symbols} ${optimization} -std=c++20 -fexceptions -ferror-limit=0 -o ${binFolder}/builder_tests_${config} ${sourceFiles} ${defines} ${includes} ${libPaths} ${libs} ${warningLevels} ${ignoreWarnings} -Wl,-rpath=$binFolder"
 echo ${args}
 ${args}

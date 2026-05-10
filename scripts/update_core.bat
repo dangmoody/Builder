006fae2520460acebe91f7ac75d15d91aadf04bc
Dan Moody <daniel.guy.moody@gmail.com>
2026-05-10 23:29:45 +0100

    i need this for the minute

---
+++ scripts/update_core.bat
@@ -0,0 +1,26 @@
+@echo off
+
+pushd %~dp0
+
+builder ..\\..\\core\\build.cpp --config=core
+:: builder ..\\..\\core\\build.cpp --config=core --release
+
+echo Copying core include folder...
+robocopy /e ..\\..\\core\\include      ..\\src\\core\\include
+
+echo Copying core src folder...
+robocopy /e ..\\..\\core\\src          ..\\src\\core\\src
+
+echo Copying core.dll...
+robocopy    ..\\..\\core\\bin\\debug ..\\3rdparty\\core core.dll
+
+echo Copying core.lib...
+robocopy    ..\\..\\core\\bin\\debug ..\\3rdparty\\core core.lib
+
+echo Copying core.pdb...
+robocopy    ..\\..\\core\\bin\\debug ..\\3rdparty\\core core.pdb
+
+echo Done.
+echo.
+
+popd

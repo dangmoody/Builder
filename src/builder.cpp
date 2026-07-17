4c2a9ec8b38b2d9b41227bce6a8d14584483a4e7
dan <daniel.guy.moody@gmail.com>
2026-07-17 16:10:26 +0100

    start release candidates

--- src/builder.cpp
+++ src/builder.cpp
@@ -1379,7 +1379,7 @@ int BuilderMain( const int firstArg, int argc, const char * const * argv ) {
 		}
 	};
 
-	printf( "Builder v%d.%d.%d\n\n", BUILDER_VERSION_MAJOR, BUILDER_VERSION_MINOR, BUILDER_VERSION_PATCH );
+	printf( "Builder v%d.%d.%d RC0\n\n", BUILDER_VERSION_MAJOR, BUILDER_VERSION_MINOR, BUILDER_VERSION_PATCH );
 
 	buildContext_t context = {};
 	context.allocator = linear_allocator_create( MEM_KILOBYTES( 128 ) );

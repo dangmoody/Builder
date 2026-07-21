9a8e26cf73566e17de5870c1d5fffd315cf2becd
Dan Moody <daniel.guy.moody@gmail.com>
2026-07-21 21:34:14 +0100

    remove the old core MIT header, its all builder source now

--- src/int_types.h
+++ src/int_types.h
@@ -1,9 +1,9 @@
 /*
 ===========================================================================
 
-Core
+Builder
 
-Copyright (c) 2025 - present Dan Moody
+Copyright (c) 2025 Dan Moody
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

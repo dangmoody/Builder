7a8071042d36fb719f6a7cef88ebd6cad8310bbb
Dan Moody <daniel.guy.moody@gmail.com>
2026-08-07 21:38:03 +0100

    that works, but we probably need to formalise it into builder.h

--- tests/test_build_single_file/build.c
+++ tests/test_build_single_file/build.c
@@ -1,8 +1,6 @@
 #define BUILDER_IMPLEMENTATION
 #include "../../builder.h"
 
-#define STRING_LIST( ... ) (const char *[]) { __VA_ARGS__, NULL }
-
 int main( int argc, char **argv ) {
 	Builder_RebuildSelf( argc, argv );
 
@@ -10,9 +8,10 @@ int main( int argc, char **argv ) {
 
 	BuildConfig config = {
 		.binaryName		= "test_build_single_file",
-		.sourceFiles = STRING_LIST(
-			"main.c"
-		),
+		.sourceFiles = (const char *[]) {
+			"main.c",
+			NULL
+		},
 	};
 
 	AddBuildConfig( &options, &config );
